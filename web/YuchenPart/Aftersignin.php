f82b64558b315516e86a4cf23817ce3f0d1511c3
Yuchen-TNG <tngyc-wm24@student.tarc.edu.my>
2025-04-10 18:58:52 +0800

    change

--- web/YuchenPart/Aftersignin.php
+++ web/YuchenPart/Aftersignin.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <body>
-            <div class="spacezheyong" >
-                
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Aftersigin.css">
+    <title>Square Example</title>
+</head>
+<body>
+    <div class="container">
+        <div class="left-section">
+            <h1>Memberlist</h1>
+            <div class="SPACE01">
+                <a href="Content.html">
+                    <img src="user_icon_007.jpg" class="Clickanimation" alt="User Icon">
+                </a>
             </div>
-            <h1>you are correct</h1>
-            
-        </body>
-    </head>
+        </div>
+
+        <div class="right-section">
+            <h1>Product</h1>
+            <div class="SPACE02">
+                <a href="Content.html">
+                    <img src="Product_sample_icon_picture.png" class="Clickanimation" alt="Product Image">
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
