6aacebd3dd4ea773fabcd40938e34a783c7448ea
Yuchen-TNG <tngyc-wm24@student.tarc.edu.my>
2025-04-10 21:11:17 +0800

    a

--- web/YuchenPart/logout.php
+++ web/YuchenPart/logout.php
@@ -2,6 +2,6 @@
 session_start();
 session_unset(); // 清空所有 session 变量
 session_destroy(); // 销毁 session
-header("Location: index.php"); // 跳转回首页
+header("Location: c:\xampp\htdocs\a\Website_assignment\web\TanKZpart\index.php"); // 跳转回首页
 exit();
 ?>
